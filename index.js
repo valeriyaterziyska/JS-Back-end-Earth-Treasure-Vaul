066e0255740652cb89becae17be36396315f6e86
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-16 10:28:02 +0200

    Install and config handblebars

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 //TODO: Change the port
 const PORT = 5000;
 
@@ -8,8 +9,13 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
-app.use(routes);
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use(routes);
 
 
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
