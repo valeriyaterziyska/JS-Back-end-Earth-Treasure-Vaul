f5b51cade67c46f8324fdfab8278899e357ccf44
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-16 12:35:57 +0200

    Add user model

--- index.js
+++ index.js
@@ -20,13 +20,11 @@ app.set('view engine', 'hbs');
 
 app.use(routes);
 
-
 //TODO: change database name
-mongoose.connect(`mongodb://localhost27017/${dbName}`);
+mongoose.connect(`mongodb://localhost:27017/${dbName}`);
 mongoose.connection.on('connected', () => console.log(`DB is connected!`));
-mongoose.connection.on('disconnected', () => console.log(`DB is disconnected!`));
+// mongoose.connection.on('disconnected', () => console.log(`DB is disconnected!`));
 mongoose.connection.on('error', (err) => console.log(`DB error ${err}`));
-
 
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
 
