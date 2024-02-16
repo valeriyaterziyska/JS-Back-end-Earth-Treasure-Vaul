9f2ddb1a60e15abd5a8b30428cc792a26bd94ac3
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-16 10:19:31 +0200

    Add body-parser middleware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
