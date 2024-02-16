17f83b58b1289684e9b356f96a4abdee5c5a267d
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-16 10:23:08 +0200

    Add routes

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 //TODO: Change the port
 const PORT = 5000;
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+
 
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
 
